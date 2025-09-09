3a5a5be697eddbb7897b74713ad179ae8e29d3fc
Luiz de Figueiredo <luiz.figueiredo@aluno.ifsp.edu.br>
2025-09-09 10:24:49 -0300

    Tela de catálogo de produtos

--- Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
+++ Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
@@ -1,41 +1,64 @@
 import React from "react";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-import { RootStackParamList, TabParamList } from './types';
+import { RootStackParamList, TabParamList } from "./types";
 
 // Telas do app - área não logada.
 import HomeScreen from "../screens/HomeScreen";
-// importar depois que implementar: DetailsScreen, SettingsScreen
 import RegisterScreen from "../screens/RegisterScreen";
 import LoginScreen from "../screens/LoginScreen";
+import CatalogScreen from "../screens/catalog/catalogScreen";
+
+// TODO: Substituir por uma tela real de configurações
+const SettingsScreen = HomeScreen; 
+// TODO: Substituir por uma tela real de detalhes
+const DetailsScreen = HomeScreen; 
 
 const AppStack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
 
 function TabNavigator() {
-    return (
-        <Tab.Navigator
-            screenOptions={({route, navigation}) => ({
-              tabBarIcon: ({ color, focused, size}) => {
-                let iconName;
-                if (route.name === "Home") {
-                  iconName = focused ? "home" : "home";
-                }
-                return <FontAwesome name={iconName} size={size} color={color} />
-              },
-              tabBarActiveTintColor: "red",
-              tabBarInactiveTintColor: "grey",
-              headerShown: false,
-            })}
-          >
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Settings" component={HomeScreen} />
-            <Tab.Screen name="Register" component={RegisterScreen} />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size, focused }) => {
+          let iconName: keyof typeof FontAwesome.glyphMap = "question";
+
+          if (route.name === "Catalog") {
+            iconName = "tags";
+          } else if (route.name === "Settings") {
+            iconName = "cog";
+          } else if (route.name === "Register") {
+            iconName = "user-plus";
+          }
+
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "grey",
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen
+        name="Catalog"
+        component={CatalogScreen}
+        options={{ title: "Menu" }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ title: "Configurações" }}
+      />
+      <Tab.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ title: "Cadastrar" }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 function StackNavigator() {
@@ -48,10 +71,10 @@ function StackNavigator() {
       />
       <AppStack.Screen
         name="Details"
-        component={HomeScreen}
-        options={{ title: 'Detalhes' }}
+        component={DetailsScreen}
+        options={{ title: "Detalhes" }}
       />
-      <AppStack.Screen 
+      <AppStack.Screen
         name="Login"
         component={LoginScreen}
         options={{ title: "Acessar" }}
@@ -61,7 +84,5 @@ function StackNavigator() {
 }
 
 export default function AppNavigator() {
-  return (
-    <StackNavigator />
-  );
-};
+  return <StackNavigator />;
+}

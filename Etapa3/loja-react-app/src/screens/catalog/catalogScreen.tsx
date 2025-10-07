27641b7f08bf05a70a9b8b16c6eafb80738b532b
Luiz de Figueiredo <luiz.figueiredo@aluno.ifsp.edu.br>
2025-10-07 07:52:24 -0300

    Carrinho de compras salvado o estado dos items

--- Etapa3/loja-react-app/src/screens/catalog/catalogScreen.tsx
+++ Etapa3/loja-react-app/src/screens/catalog/catalogScreen.tsx
@@ -1,13 +1,16 @@
-import React, { useContext, useState, useEffect } from "react"; // alterado
+import React, { useState, useEffect } from "react"; // alterado
 import { View, Text, FlatList, StyleSheet} from 'react-native';
 
-import CatalogCard from "../catalog/catalogCard";
+import CatalogCard from "./CatalogCard";
 
 // Todo: importar o serviço de recuperação do catalog
 import { getCatalog } from '../../services/catalogService'; // novo
 
+import { useShop } from "../../contexts/ShopContext";
+
 const CatalogScreen = ({navigation} : any) => {
     const [catalog, setCatalog] = useState<any[]>([]); // novo
+    const { addToCart } = useShop();
 
     // bloco novo
     useEffect(() => {
@@ -27,6 +30,7 @@ const CatalogScreen = ({navigation} : any) => {
     const handleBuyPress = (product : any) => {
         // 1 - Adicionar ao carrinho
         // 2 - Ir para a tela do carrinho
+        addToCart(product);
         console.log(product);
     };
 
